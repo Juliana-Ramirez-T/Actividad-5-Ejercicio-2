f61f9ab1c0a4b9519a6631daad9c10d6a210e384
Juliana <Juliana@LAPTOP-D1IJKFMU>
2023-11-10 22:06:46 -0500

    Updated

---
+++ src/figuras/Principal.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package figuras;
+public class Principal {
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; /* Define la ventana principal */
+        miVentanaPrincipal= new VentanaPrincipal(); /* Crea la ventana principal */
+        miVentanaPrincipal.setVisible(true); /* Establece la ventana como visible */
+        // Establece que la ventana no puede cambiar su tamaño
+        miVentanaPrincipal.setResizable(false);
+    }
+}
